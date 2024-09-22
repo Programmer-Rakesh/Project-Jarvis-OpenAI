a3ca27f55fd5f5ce6f7a51af4cea788425348cda
Programmer-Rakesh <rakeshaps8866@gmail.com>
2024-09-22 12:49:01 +0530

    New Codes

--- Jarvis2.py
+++ Jarvis2.py
@@ -46,30 +46,30 @@ def wishme():
     speak("and today's date is")
     date()
     speak("Jarvis at your service. please tell me how may I help you ?")
+  
+wishme()
 
-wishme()  
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+    try:
+        print("Recongnizing...")
+        query = r.recognize_google(audio, language= 'en-in')
+        print(query)
+    except Exception as e:
+        print(e)
+        speak("Say that again please...")
+        return "none"
+    return query
 
-# def takeCommand():
-#     r = sr.Recognizer()
-#     with sr.Microphone() as source:
-#         print("Listening...")
-#         r.pause_threshold = 1
-#         audio = r.listen(source)
-#     try:
-#         print("Recongnizing...")
-#         query = r.recognize_google(audio, language= 'en-in')
-#         print(query)
-#     except Exception as e:
-#         print(e)
-#         speak("Say that again please...")
-#         return "none"
-#     return query
+takeCommand()
 
-# takeCommand()
-
-# if __name__=="__main__":
-#     with True:
-#         query = takeCommand().lower()
-#         if 'Jarvis' in query:
-#             print("Yes sir")
-#             speak("Yes sir")
+if __name__=="__main__":
+    with True:
+        query = takeCommand().lower()
+        if 'Jarvis' in query:
+            print("Yes sir")
+            speak("Yes sir")
